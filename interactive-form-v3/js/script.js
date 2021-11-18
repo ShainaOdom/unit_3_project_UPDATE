6f52c6b8a24bfefe75c786a18bdbae19c9d06041
Shaina <shainabodom@gmail.com>
2021-11-18 12:33:12 -0600

    up to #6 complete
    Job Role, T-shirt Info, and Register for Activities are all working as they should.
    
    Total cost is totaled as checkboxes are checked or unchecked.

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -2,7 +2,7 @@
 const name = document.querySelector('#name');
 name.focus();
 //text field that will be revealed when the 'Other' option is selected from 'Job Role'
-const other = document.querySelector('#other-title');
+const other = document.querySelector('#other-job-role');
 //Job Role select
 const jobRole = document.querySelector('#title');
 
@@ -28,8 +28,7 @@ jobRole.addEventListener('change', (e) => {
   colors.disabled = true;
   colorOptions[0].textContent = 'Please select a T-shirt color';
 
-//supposed to give shirt color options depending on which design is selected
-//NOT WORKING
+//event listener to give shirt color options depending on which design is selected
   design.addEventListener('change', (e) => {
       colors.disabled = false;
 
@@ -38,17 +37,27 @@ jobRole.addEventListener('change', (e) => {
         const dataTheme = colorOptions[i].getAttribute('data-theme'); //https://stackoverflow.com/questions/65942968/storing-and-retrieving-a-theme-in-javascript-from-a-data-attribute
 
 
-
           if (designSelection === dataTheme) {
               colorOptions[i].hidden = false;
               colorOptions[i].setAttribute('selected', true);
             } else {
               colorOptions[i].hidden = true;
               colorOptions[i].removeAttribute('selected', false);
-              }
-
-              console.log(dataTheme);
-              console.log(designSelection);
-
+            }
       }
+  });
+
+//variables to reference the 'Register for Activites' element, 'Total $' element, and total cost.
+  const activities = document.querySelector('#activities');
+  const activitiesCost = document.querySelector('#activities-cost');
+  let totalCost = 0;
+//event listener to add the Total cost depening which checkboxes are selected.
+  activities.addEventListener('change', (e) => {
+      const dataCost = e.target.getAttribute('data-cost');
+        if(e.target.checked === true){
+            totalCost += parseInt(dataCost);
+        } else(
+            totalCost -= parseInt(dataCost)
+        );
+      activitiesCost.innerHTML = 'Total: $' +totalCost;
   });
